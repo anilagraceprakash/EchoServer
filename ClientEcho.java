ac71187a42702ae7ba772108934ddf0e536eaf7e
anilagraceprakash <anilagracep@gmail.com>
2018-03-24 12:35:05 +0530

    client file modified

--- ClientEcho.java
+++ ClientEcho.java
@@ -1,4 +1,16 @@
 import java.io.*;
 import java.net.*;
 
-
+public class ECHO_cli
+{
+public static void main(String args[]) throws Exception
+{
+ try 
+  {
+   int Port;
+   BufferedReader Buf =new BufferedReader(newInputStreamReader(System.in));
+   System.out.print(" Enter the Port Address : " );
+   Port=Integer.parseInt(Buf.readLine());
+   Socket sok=new Socket("localhost",Port);
+   if(sok.isConnected()==true)
+    System.out.println(" Server Socket is Connected Succecfully. ");
